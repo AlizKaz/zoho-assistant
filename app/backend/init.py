57fc57bd853cbd3ba19809955fb98a69b3b24fcc
Alireza <aliz.kazerani@gmail.com>
2024-07-13 22:17:48 -0400

    create an invoice

---
+++ app/backend/init.py
@@ -0,0 +1,70 @@
+import logging
+import os
+from http.client import HTTPConnection
+
+import requests
+from dotenv import load_dotenv
+import zoho_auth
+from app.backend import zoho
+
+
+def debug_requests_on():
+    """Switches on logging of the requests' module."""
+    HTTPConnection.debuglevel = 1
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+def auth_hook_factory(*factory_args, **factory_kwargs):
+    def auth_hook(response, *args, **kwargs):
+        if response.status_code == 401:
+            zoho_auth_obj = factory_kwargs.get('zoho_auth')
+            session = factory_kwargs.get('session')
+            print(f'getting 401 response code. trying to refresh access token')
+            # Refresh token
+            new_access_token = zoho_auth_obj.refresh_and_save_access_token()
+            # Update session headers with new token
+            response.request.headers['Authorization'] = f'Zoho-oauthtoken {new_access_token}'
+            # Retry original request
+            return session.send(response.request)
+    return auth_hook
+
+
+class AppInit:
+
+    def __init__(self):
+        self.config = None
+
+    def init_app(self):
+        load_dotenv()
+        if os.environ.get("debug_request") == 'on':
+            debug_requests_on()
+
+        self.config = Config()
+        self.config.init_zoho_auth()
+
+
+class Config:
+    def __init__(self):
+        self.invoice = None
+        self.accounts_server_url = None
+        self.zoho_auth = None
+
+    def init_zoho_auth(self):
+        self.zoho_auth = zoho_auth.ZohoAuth(
+            client_id=os.environ.get("zoho_client_id"),
+            client_secret=os.environ.get("zoho_client_secret"),
+            authorization_code=os.environ.get("zoho_authorization_code"),
+            auth_store_filepath=os.environ.get("auth_store_filepath"),
+            accounts_server_url=os.environ.get('zoho_accounts_server_url'))
+
+        session = requests.session()
+        session.hooks['response'].append(auth_hook_factory(zoho_auth=self.zoho_auth, session=session))
+
+        access_token = self.zoho_auth.get_access_token_if_not_exists()['access_token']
+
+        self.invoice = zoho.Invoice(os.environ.get("zoho_books_api_server_url"), access_token, session)
--- docs/zoho_api_troubleshooting.md
+++ docs/zoho_api_troubleshooting.md
@@ -2,5 +2,18 @@
    - url: /oauth/v2/token
    - error: invalid_code 
    - solution: [Generate a new authorization code](https://www.zoho.com/accounts/protocol/oauth/self-client/authorization-code-flow.html) put in zoho_grant_token in .env 
-     - Scope: ZohoBooks.fullaccess.all
+     - Scope: 
+2. JSON is not well formed
+   - url: any url 
+   - error: JSON is not well formed
+   - code: 1038
+   - solution: Zoho expects to see a json string in the body as opposed to string parameters separated by `&` character.
+     - wrong: 
+     ```python
+     requests.post(api_url, data=payload, headers=headers)
+     ```
+     - correct: 
+     ```python
+     requests.post(api_url, json=payload, headers=headers)
+     ```
      
